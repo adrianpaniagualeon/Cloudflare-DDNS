d7be5e4c9327eaf85d8b54f3fab23188200434c6
Adrián <github@adrianpaniagua.es>
2021-03-08 16:54:27 +0100

    First Version

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Cloudflare DDNS
- 
+
+## ¿Qué hace este script?
+Este script permite la actualización de la IP pública de la maquina que lo ejecuta en los servidores DNS de Cloudflare. Para evitar sobrecargar la API de Cloudflare, se ejecuta cada 5 minutos hasta que el usuario decida parar el servicio.
+
+## ¿Cómo puedo colaborar?
+Existen dos formas de colaborar:
+- Añadiendo nuevas funcionalidades al bot mediante _pull-request_. 
+- Aportación económica: Puedes aportar tu granito de arena por [Paypal](https://paypal.me/panleoad)
+- Invitarme a un café: Me puedes invitar a un café a través de [Ko-Fi](https://ko-fi.com/adrianpaniagualeon)
+
+## ¿Necesita alguna librería externa?
+Si, este script necesita la librería [Requests](https://requests.readthedocs.io/en/master/). Puedes instalarla con el siguiente comando "pip install requests"
+
+## ¿Qué datos necesito para autenticarme?
+|DATO|INFORMACIÓN|
+|-|-|
+|CLOUDFLARE_EMAIL|CORREO ELECTRÓNICO UTILIZADO PARA REGISTRARTE EN CLOUDFLARE|
+|CLOUDFLARE_CLAVE|GLOBAL API KEY DE CLOUDFLARE. Puedes encontrarla en [https://dash.cloudflare.com/profile/api-tokens](https://dash.cloudflare.com/profile/api-tokens)|
+|DOMAIN|DOMINIO QUE QUEREMOS ACTUALIZAR. En caso de querer actualizar cv.adrianpaniagua.es **SOLO** introduciremos adrianpaniagua.es|
+|SUBDOMAIN|SUBDOMINIO QUE QUEREMOS ACTUALIZAR. En caso de querer actualizar blog.adrianpaniagua.es **SOLO** introduciremos adrianpaniagua.es. Si queremos actualizar el dominio principal, dejaremos este campo vacio|
+
+Tienes que modificar el archivo ddns.py con tus datos para que el script funcione. Es muy importante no incluir espacios para que todo funcione correctamente.
---
+++ ddns.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-            
+import requests
+import os
+import time
+import json
+
+
+#####################################################################
+#                           VARIABLE                                #
+#####################################################################
+CLOUDFLARE_EMAIL = ""                   
+CLOUDFLARE_CLAVE = ""
+DOMAIN = ""
+SUBDOMAIN = ""
+#####################################################################
+
+
+
+headers = {"X-Auth-Email":CLOUDFLARE_EMAIL, "X-Auth-Key":CLOUDFLARE_CLAVE, "Content-Type": "application/json"}
+if (SUBDOMAIN == ""):
+	subdomain = DOMAIN
+else:
+	subdomain = SUBDOMAIN+"."+DOMAIN
+
+
+def get_ip():
+	global my_ip
+
+	my_ip = requests.get('http://icanhazip.com/').text
+	my_ip = my_ip.strip()
+
+def get_zones():
+	global my_zone_name
+	global my_zone_id
+
+	url = "https://api.cloudflare.com/client/v4/zones/"
+	zones = requests.get(url,headers=headers).text
+	open('zones.json', 'w').write(zones)
+	f = open('zones.json')
+	json_file = json.load(f)
+	json_str = json.dumps(json_file)
+	zonas = json.loads(json_str)
+	zones_number = zonas['result_info']['count']
+	for i in range (zones_number):
+		my_zone_name = zonas['result'][i]['name']
+		if (my_zone_name == DOMAIN):
+			my_zone_id = zonas['result'][i]['id']
+			break
+		else:
+			i = i+1
+
+
+def get_dns():
+	global my_dns_name
+	global my_dns_id
+	global my_dns_ip
+	global my_dns_proxied
+
+	url = "https://api.cloudflare.com/client/v4/zones/"+my_zone_id+"/dns_records?per_page=100"
+	dns = requests.get(url,headers=headers).text
+	open('dns.json', 'w').write(dns)
+	f = open('dns.json')
+	json_file = json.load(f)
+	json_str = json.dumps(json_file)
+	dns = json.loads(json_str)
+	dns_number = dns['result_info']['count']
+	for i in range (dns_number):
+		my_dns_name = dns['result'][i]['name']
+		if (my_dns_name == subdomain):
+			my_dns_id = dns['result'][i]['id']
+			my_dns_ip = dns['result'][i]['content']
+			my_dns_proxied = dns['result'][i]['proxied']
+			break
+		else:
+			i = i+1
+
+
+def change_dns():
+	if (my_dns_ip == my_ip):
+		print ("THE IP ADDRESS HAS NOT CHANGED")
+	else:
+		parametros ={"type":"A","name":subdomain,"content":my_ip,"ttl":120,"proxied":my_dns_proxied}
+		url = "https://api.cloudflare.com/client/v4/zones/"+my_zone_id+"/dns_records/"+my_dns_id
+		r = requests.patch(url, json=parametros, headers = headers)
+		print ("THE IP ADDRESS HAS CHANGED")
+
+
+while True:
+	print ("-------------------------------------------")
+	print ("WEB ["+subdomain+"]")
+	get_ip()
+	print ("CURRENT IP ["+my_ip+"]")
+	get_zones()
+	get_dns()
+	print ("CLOUDLFARE IP ["+my_dns_ip+"]")
+	change_dns()
+	print ("WAITING 5 MINUTES BEFORE THE NEXT EXECUTION")
+	print ("-------------------------------------------\n\n")
+	time.sleep(300)
